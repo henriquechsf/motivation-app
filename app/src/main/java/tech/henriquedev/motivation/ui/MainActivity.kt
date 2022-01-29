56d98dba8843f30a042e74e30ce790755bf82d6f
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-01-29 11:46:09 -0300

    handle color on click menu

--- app/src/main/java/tech/henriquedev/motivation/ui/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/motivation/ui/MainActivity.kt
@@ -2,12 +2,13 @@ package tech.henriquedev.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import tech.henriquedev.motivation.R
 import tech.henriquedev.motivation.infra.MotivationConstants
 import tech.henriquedev.motivation.infra.SecurityPreferences
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mSecurityPreferences: SecurityPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,48 @@ class MainActivity : AppCompatActivity() {
 
         mSecurityPreferences = SecurityPreferences(this)
         textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+        buttonNewFrase.setOnClickListener(this)
+        imageAll.setOnClickListener(this)
+        imageHappy.setOnClickListener(this)
+        imageMorning.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        val listFilters = listOf(R.id.imageAll, R.id.imageHappy, R.id.imageMorning)
+
+        if (id == R.id.buttonNewFrase) {
+            handleNewFrase()
+        } else if (id in listFilters) {
+            handleFilter(id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+        imageAll.setColorFilter(resources.getColor(R.color.white))
+        imageHappy.setColorFilter(resources.getColor(R.color.white))
+        imageMorning.setColorFilter(resources.getColor(R.color.white))
+
+        when (id) {
+            R.id.imageAll -> {
+                imageAll.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+            R.id.imageHappy -> {
+                imageHappy.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+            R.id.imageMorning -> {
+                imageMorning.setColorFilter(resources.getColor(R.color.colorAccent))
+            }
+        }
+    }
+
+    private fun handleNewFrase() {
+        TODO("Not yet implemented")
     }
 }
