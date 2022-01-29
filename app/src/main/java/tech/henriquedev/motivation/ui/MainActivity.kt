ccb952099e1a0ee77fe52b7b6a8c56d33e2dcf00
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-01-29 12:22:52 -0300

    handle new messages on screen

--- app/src/main/java/tech/henriquedev/motivation/ui/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/motivation/ui/MainActivity.kt
@@ -7,20 +7,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 import tech.henriquedev.motivation.R
 import tech.henriquedev.motivation.infra.MotivationConstants
 import tech.henriquedev.motivation.infra.SecurityPreferences
+import tech.henriquedev.motivation.repository.Mock
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mSecurityPreferences: SecurityPreferences
+    private var mPhraseFilter: Int = MotivationConstants.PHRASEFILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mSecurityPreferences = SecurityPreferences(this)
-        textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
-
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+
+        mSecurityPreferences = SecurityPreferences(this)
+        textName.text ="Ola " + mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+        // Logica inicial de selecao
+        imageAll.setColorFilter(resources.getColor(R.color.colorAccent))
+        handleNewFrase()
 
         buttonNewFrase.setOnClickListener(this)
         imageAll.setOnClickListener(this)
@@ -48,17 +54,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (id) {
             R.id.imageAll -> {
                 imageAll.setColorFilter(resources.getColor(R.color.colorAccent))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.ALL
             }
             R.id.imageHappy -> {
                 imageHappy.setColorFilter(resources.getColor(R.color.colorAccent))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
             }
             R.id.imageMorning -> {
                 imageMorning.setColorFilter(resources.getColor(R.color.colorAccent))
+                mPhraseFilter = MotivationConstants.PHRASEFILTER.MORNING
             }
         }
     }
 
     private fun handleNewFrase() {
-        TODO("Not yet implemented")
+        textFrase.text = Mock().getPhrase(mPhraseFilter)
     }
 }
